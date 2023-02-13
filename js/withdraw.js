b76ff422da1ceb13fac5153bf308310ed1fed097
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-02-13 19:33:05 +0600

    commit updated

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,22 +5,34 @@ document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmountString = withdrawField.value;
   //* also make sure convert input a number by using parseFloat
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+  //* step 7: clear the input field
+  withdrawField.value = "";
+  if (isNaN(newWithdrawAmount)) {
+    alert("Please provide a valid number");
+    return;
+  }
   //* step 3: get previous withdraw total
   const withdrawTotalElement = document.getElementById("withdraw-total");
   const previousWithdrawTotalString = withdrawTotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-  //* step 4: calculate total withdraw amount
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  //* set total withdraw amount
-  withdrawTotalElement.innerText = currentWithdrawTotal;
+
   //* step 5: get the previous total
   const balanceTotalElement = document.getElementById("balance-total");
   const previousBalanceTotalString = balanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  if (newWithdrawAmount > previousBalanceTotal) {
+    alert("Baap er Bank a ato TAKA nai!!!:(");
+    return;
+  }
+  //* step 4: calculate total withdraw amount
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  //* set total withdraw amount
+  withdrawTotalElement.innerText = currentWithdrawTotal;
+
   //* step 6: calculate new balance total
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
   //* set new balance total
   balanceTotalElement.innerText = newBalanceTotal;
-  //* step 7: clear the input field
-  withdrawField.value = "";
 });
